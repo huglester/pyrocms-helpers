45b1072fad88e97f125bbbacd403d515fd3533d4
huglester <huglester@gmail.com>
2013-07-25 15:49:31 +0300

    add fluent for connector

---
+++ lib/fluent.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// how to replace this with more elegant solution?
+require_once('connection.php');
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Fluent extends Illuminate\Database\Query\Builder {
+
+
+}
