7337982f30109cf15c0afe13b174da219ddfe800
huglester <huglester@gmail.com>
2013-09-24 16:00:47 +0300

    image sizes fixes

--- lib/eloquent.php
+++ lib/eloquent.php
@@ -73,6 +73,8 @@ class Eloquent extends Illuminate\Database\Eloquent\Model {
 			$module_name = str_replace('_m', '', get_called_class());
 			$model_name = ucfirst($module_name).'Setting_m';
 
+			log_message('Error', 'image dynamic...'.$model_name);
+
 			ci()->load->model(array(
 				$module_name.'/'.strtolower($model_name),
 			));
