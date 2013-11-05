c816d1419693abd239c22daea2266deb32d4dfcd
huglester <huglester@gmail.com>
2013-11-05 16:56:38 +0200

    implemented stripes

--- lib/imagineresizer.php
+++ lib/imagineresizer.php
@@ -300,6 +300,112 @@ class ImagineResizer
 		return $this->file->getFileName();
 	}
 
+	public function logoStripe($destination, $dest_width = null, $dest_height = null, $quality = 90, $vertical = true)
+	{
+		if ( ! $dest_width and ! $dest_height)
+		{
+			throw new Exception("\$dest_width or \$dest_height is required.\n");
+		}
+
+		$imagine = new Imagine\Gd\Imagine();
+		$image = $imagine->open($this->file);
+		
+		// original size
+		$srcBox = $image->getSize();
+
+		( ! $dest_width and $dest_height) and $dest_width = $this->autoWidth($dest_height);
+		( ! $dest_height and $dest_width) and $dest_height = $this->autoHeight($dest_width);
+
+		$box = new Box($dest_width, $dest_height);
+
+		$new_filename = pathinfo($destination, PATHINFO_FILENAME);
+		$new_extention = pathinfo($destination, PATHINFO_EXTENSION);
+
+		$filename = $this->file->getFileName();
+
+		// extension exists - change destination filename
+		if ($new_extention and $new_filename)
+		{
+			$filename = $new_filename.'.'.$new_extention;
+
+			// remove filename from destination
+			$destination = substr($destination, 0, -strlen($filename));
+		}
+
+		$destination = rtrim($destination, '/').'/';
+
+		if ($srcBox->getWidth() < $dest_width and $srcBox->getHeight() < $dest_height)
+		{
+			$dest = FCPATH.$destination.$this->file->getFileName();
+
+			// only if paths are different
+			// or if we have a watermark, and want to overwrite original photo...
+			if ($dest != $this->full_path or $this->watermark)
+			{
+				// do we need to apply a watermark?
+				if ($this->watermark)
+				{
+					// we get position for our watermark
+					$watermark_point = $this->getWatermarkPos($box, $srcBox);
+					$image->paste($this->watermark, $watermark_point);
+				}
+
+				// we use this, since we want to preserve watermark, quality. Can't simply copy()
+				$image
+					->save($destination.$filename, array('quality' => $quality));
+
+				@chmod($dest, 0666);
+			}
+		}
+		else
+		{
+			//we scale on the smaller dimension
+			if ($srcBox->getWidth() > $srcBox->getHeight()) {
+				$width  = $srcBox->getWidth() * ($box->getHeight() / $srcBox->getHeight());
+				$height =  $box->getHeight();
+				//we center the crop in relation to the width
+				$cropPoint = new Point((max($width - $box->getWidth(), 0))/2, 0);
+			} else {
+				$width  = $box->getWidth();
+				$height =  $srcBox->getHeight() * ($box->getWidth() / $srcBox->getWidth());
+				//we center the crop in relation to the height
+				$cropPoint = new Point(0, (max($height - $box->getHeight(),0)) / 2);
+			}
+
+			$image = $image->thumbnail($box, \Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND);
+
+			$image
+				//->crop($cropPoint, new Box($width, $height))
+				->save($destination.$filename, array('quality' => $quality));
+		}
+
+		$image_gray = $imagine->open($destination.$filename);
+		$image_gray->effects()->grayscale();
+
+		if ($vertical)
+		{
+			$box_stripe = new Box($dest_width, $dest_height * 2);
+			$offset_x = 0;
+			$offset_y = $dest_height;
+		}
+		else
+		{
+			$box_stripe = new Box($dest_width * 2, $dest_height);
+			$offset_x = $dest_width;
+			$offset_y = 0;
+		}
+
+		$container = $imagine->create($box_stripe);
+
+		$container->paste($image, new Point(0, 0)); // original image
+		$container->paste($image_gray, new Point($offset_x, $offset_y)); // grayscale image
+		$container->save($destination.$filename, array('quality' => $quality));
+
+		@chmod($destination.$filename, 0666);
+
+		return $this->file->getFileName();
+	}
+
 	protected function validateImage()
 	{
 		$mime = $this->file->getMimeType();
